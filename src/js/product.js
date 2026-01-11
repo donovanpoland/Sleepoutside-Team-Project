2c88fc8f56bdf553ec5ee6ca0cd7944b483cd3cd
mrshawnh <shenrie5721@gmail.com>
2026-01-11 08:21:33 -0700

    sh-individual1 created

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get cart items from local storage or init an empty array
+  const cartItems = getLocalStorage("so-cart") || [];
+  // Add product to cart
+  cartItems.push(product);
+  // Save back to storage
+  setLocalStorage("so-cart", cartItems);
 }
-// add to cart button event handler
+
+// Add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+// Add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
