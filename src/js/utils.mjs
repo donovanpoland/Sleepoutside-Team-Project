418917e1e823ecc8680647f7e6a9ef8a62d6dc91
Donovan <djamespoland@gmail.com>
2026-01-17 13:57:01 -0700

    dynamicly listing all tents from tents.json(images for last 2 currently missing) added main module to home index.html, removed static prodcuts listed and inserting dynamicly listed items instead. changed path for product data, made getParam function dynamic. updated package.json to contain module for new vite logic per documentation.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,17 +2,17 @@
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
+
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -23,10 +23,11 @@ export function setClick(selector, callback) {
 }
 
 // get the product id from the query string
+// get the product id from the query string
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product');
+  const product = urlParams.get(param);
   return product;
 }
 
