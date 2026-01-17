e9e2bf48cf4667eacc461f4dcb02fb1398524236
EzeAlba <ezealba2@gmail.com>
2026-01-17 02:42:13 -0300

    Individual activity completed. Also edited the path on producData to make it work on production link. Tested with the npm run preview command.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,9 +22,20 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
+// get the product id from the query string
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get('product');
   return product;
+}
+
+// render a list of items using a template function
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
