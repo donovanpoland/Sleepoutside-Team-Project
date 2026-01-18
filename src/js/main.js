c328df4eb882014f7e4679144a1237af65927f37
sydney818 <sydney818@gmail.com>
2026-01-17 19:49:48 -0700

    Add configurable alert banner to index page

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { updateCartCount } from "./utils.mjs";
+import Alert from "./Alert.js";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, element);
 productList.init();
 updateCartCount();
+
+const alert = new Alert("/json/Alerts.json");
+alert.init();
+  
+
