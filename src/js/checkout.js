67d61fab85f4aa23b885b5a9bc007050c8017349
Donovan <djamespoland@gmail.com>
2026-01-28 20:47:24 -0700

    changes

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -4,7 +4,7 @@ import { updateCartCount, loadHeaderFooter } from "./utils.mjs";
 // insert header and footer - wait for it to finish before updating cart count
 document.addEventListener("DOMContentLoaded", () => {
   loadHeaderFooter().then(() => {
-    const cart = new ShoppingCart()
+    const cart = new ShoppingCart();
 
     // update cart count after header is loaded
     updateCartCount();
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
-import { updateCartCount, loadHeaderFooter, getParam } from "./utils.mjs";
+// import ProductData from "./ProductData.mjs";
+// import ProductList from "./ProductList.mjs";
+import { updateCartCount, loadHeaderFooter } from "./utils.mjs";
 
 /* Get category from URL parameter, default to "tents"
 const category = getParam("category") || "tents";
