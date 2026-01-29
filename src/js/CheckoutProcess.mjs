2be960daab545199ed090093f9b4f22ca7ff78a9
Donovan <djamespoland@gmail.com>
2026-01-28 20:22:31 -0700

    created checkout module, updated checkout page to get data from form, created checkout object, get subtotal, total, and taxes, added checkout button to cart page, minimal styling of form.

---
+++ src/js/CheckoutProcess.mjs
@@ -0,0 +1,43 @@
+import { getLocalStorage } from "./utils.mjs";
+
+
+export default class CheckoutProcess{
+  constructor(key, outputSelector) {
+    this.key = key;
+    this.outputSelector = outputSelector;
+    this.list = [];
+    this.itemTotal = 0;
+    this.shipping = 0;
+    this.tax = 0;
+    this.orderTotal = 0;
+  }
+
+  init() {
+    this.list = getLocalStorage(this.key);
+    this.calculateItemSummary();
+  }
+
+  calculateItemSubTotal() {
+    // calculate and display the total dollar amount of the items in the cart, and the number of items.
+    return subtotal = this.list.reduce(
+        (sum, item) => sum + item.FinalPrice * (item.Quantity), 0,);
+  }
+
+  calculateOrderTotal() {
+    // calculate the tax and shipping amounts. Add those to the cart total to figure out the order total
+    this.tax = (this.itemTotal * 0.06);
+    this.shipping = 10.00 + ((this.list.length() -1) * 2);
+    this.orderTotal = this.shipping + this.tax;
+
+    // display the totals.
+    this.displayOrderTotals();
+  }
+
+  displayOrderTotals() {
+    // once the totals are all calculated display them in the order summary page
+    const tax = document.querySelector(`${this.outputSelector} #tax`);
+
+
+    tax.innerText = `$${this.tax.toFixed(2)}`;
+  }
+}
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,9 +1,11 @@
+import ShoppingCart from "./ShoppingCart.mjs";
 import { updateCartCount, loadHeaderFooter } from "./utils.mjs";
 
 // insert header and footer - wait for it to finish before updating cart count
 document.addEventListener("DOMContentLoaded", () => {
   loadHeaderFooter().then(() => {
-    // other startup code
+    const cart = new ShoppingCart()
+
     // update cart count after header is loaded
     updateCartCount();
   });
