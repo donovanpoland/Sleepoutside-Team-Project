5021f92f30499243d9900f4ff3fe34d72c0a1c59
EzeAlba <ezealba2@gmail.com>
2026-01-30 23:00:53 -0300

    lint and format

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,9 +1,5 @@
 import CheckoutProcess from "./CheckoutProcess.mjs";
-import {
-  updateCartCount,
-  loadHeaderFooter,
-  getLocalStorage,
-} from "./utils.mjs";
+import { updateCartCount, loadHeaderFooter } from "./utils.mjs";
 
 // insert header and footer - wait for it to finish before updating cart count
 document.addEventListener("DOMContentLoaded", () => {
